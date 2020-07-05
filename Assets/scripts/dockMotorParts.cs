27d96a86bf833bedad37ed4adee7c5077a148445
Lima <rogerio_lima@me.com>
2020-07-05 15:02:55 -0300

    Upload inicial do projeto
    Primeiro projeto funcional. Falta conexão com banco de dados

---
+++ Assets/scripts/dockMotorParts.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+using System.Linq;
+using System;
+using UnityEngine.UI;
+using TMPro;
+
+public class dockMotorParts : MonoBehaviour
+{
+[SerializeField]
+    Dockable[] m_dockable;
+
+    [SerializeField]
+    DockPosition dockPosition;
+
+    [SerializeField]
+    string objName;
+
+    [SerializeField]
+    GameObject[] desligar;
+
+    [SerializeField]
+    GameObject[] ligar;
+
+    [SerializeField]
+    GameObject instrucoes, check;
+
+    [SerializeField]
+    placarController p_controller;
+
+
+    private void Start()
+    {
+        dockPosition = gameObject.GetComponent<DockPosition>();
+        
+    }
+    void HideMeshIfDocked()
+    {
+        Debug.Log("método acionado com colisão...");
+
+        switch (objName)
+        {
+            case "eixo":
+                Debug.Log("Acionar game objects e desligar collider");
+                LigarObjetos();
+                DesligarObjetos();
+                break;
+
+            case "correia":
+                Debug.Log("Acionar game objects e desligar collider");
+                LigarObjetos();
+                DesligarObjetos();
+                break;
+
+            case "engrenagem":
+                Debug.Log("Acionar game objects e desligar collider");
+                LigarObjetos();
+                DesligarObjetos();
+                break;
+
+            case "polia":
+                Debug.Log("Acionar game objects e desligar collider");
+                LigarObjetos();
+                DesligarObjetos();
+                break;
+
+        }
+    }
+
+
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //dockPosition = collider.gameObject.GetComponent<DockPosition>();
+       
+        if (dockPosition != null && collider.gameObject.name == objName)
+        {
+
+            //objName = collider.gameObject.name;
+            Debug.Log($"{collider.gameObject.name} collided with {dockPosition.name} no meu script");
+            Debug.Log("Funcionou para o "+ objName);
+            HideMeshIfDocked();
+        }
+    }
+
+
+
+    void LigarObjetos()
+    {
+        for (int i = 0; i<ligar.Length; i++)
+        {
+            ligar[i].GetComponent<MeshRenderer>().enabled = true;
+        }
+
+        int ptos = 10;
+
+        p_controller.SetPlacar(ptos);
+    }
+
+    void DesligarObjetos()
+    {
+        for (int i = 0; i < desligar.Length; i++)
+        {
+            desligar[i].gameObject.SetActive(false);
+        }
+
+        instrucoes.gameObject.SetActive(false);
+        check.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/scripts/placarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class placarController : MonoBehaviour
+{
+    [SerializeField]
+    string nome;
+    [SerializeField]
+    int pontos;
+    [SerializeField]
+    TMP_Text placar, tripulante;
+
+    [SerializeField]
+    GameObject final;
+
+    private void Start()
+    {
+        tripulante.text = nome;
+    }
+
+    public void SetPlacar(int ptos)
+    {
+        pontos = pontos + ptos;
+        placar.text = "PONTOS: "+pontos.ToString();
+
+        if(pontos >= 40)
+        {
+            final.SetActive(true);
+        }
+    }
+}
